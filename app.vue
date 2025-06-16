a7eec1c9d252c80c33e273affe32a387f2741d3c
高立民 <616436112@qq.com>
2025-06-16 19:49:57 +0800

    feat: ttf to woff2

--- app.vue
+++ app.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+// 预加载字体文件
+useHead({
+  link: [
+    {
+      rel: "preload",
+      href: "/assets/font/Merienda_woff2/Merienda-Regular.woff2",
+      as: "font",
+      type: "font/woff2",
+      crossorigin: 'anonymous',
+    },
+  ],
+});
+</script>
+
 <template>
   <div>
     <Brief />
