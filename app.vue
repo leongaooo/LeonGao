23cb1bf67dd520915a66d6648e12238a65d82544
高立民 <616436112@qq.com>
2025-06-16 19:54:52 +0800

    feat: keywords

--- app.vue
+++ app.vue
@@ -10,6 +10,11 @@ useHead({
       crossorigin: 'anonymous',
     },
   ],
+  // Seo优化
+  meta: [
+    { name: 'keywords', content: '高立民,前端工程师,web前端开发,Web,Three.js,Render' },
+    { name: 'description', content: '高立民是一位专业的前端工程师，擅长 web 前端开发，精通 Web 技术和 Three.js 渲染技术。' }
+  ]
 });
 </script>
 
