ff39d0824408a7ffea7c66b0ece79225ed7cccbc
高立民 <616436112@qq.com>
2025-07-26 15:42:32 +0800

    feat: 背景

--- app/components/Common/FlickeringGrid.vue
+++ app/components/Common/FlickeringGrid.vue
@@ -1,208 +1,274 @@
 <template>
-    <div ref="containerRef" class="w-full h-full relative">
-        <canvas ref="canvasRef" class="pointer-events-none absolute z-1" :width="canvasSize.width"
-            :height="canvasSize.height" />
-        <div class="absolute top-0 left-0 w-full h-full z-2">
-            <slot>
-
-            </slot>
-        </div>
-
+  <div
+    ref="canvasContainerRef"
+    class="w-full h-full relative"
+    aria-hidden="true"
+  >
+    <canvas ref="canvasRef" absolute top-0 left-0 w-full h-full z-1></canvas>
+    <div class="absolute top-0 left-0 w-full h-full z-3">
+      <slot> </slot>
     </div>
+  </div>
 </template>
 
-<script lang="ts" setup>
-import { ref, onMounted, onBeforeUnmount, toRefs, computed } from "vue";
+<script setup lang="ts">
+import { useMouse, useDevicePixelRatio } from "@vueuse/core";
+import {
+  ref,
+  onMounted,
+  onBeforeUnmount,
+  watch,
+  computed,
+  reactive,
+} from "vue";
 
-interface FlickeringGridProps {
-    squareSize?: number;
-    gridGap?: number;
-    flickerChance?: number;
-    color?: string;
-    width?: number;
-    height?: number;
-    class?: string;
-    maxOpacity?: number;
-}
-
-const props = withDefaults(defineProps<FlickeringGridProps>(), {
-    squareSize: 4,
-    gridGap: 6,
-    flickerChance: 0.3,
-    color: "rgb(0, 0, 0)",
-    maxOpacity: 0.3,
-});
-
-const { squareSize, gridGap, flickerChance, color, maxOpacity, width, height } = toRefs(props);
-
-const containerRef = ref<HTMLDivElement>();
-const canvasRef = ref<HTMLCanvasElement>();
-const context = ref<CanvasRenderingContext2D>();
-
-const isInView = ref(false);
-const canvasSize = ref({ width: 0, height: 0 });
-
-// 生成一个介于 150 到 220 之间的随机数，确保颜色不太深也不太亮
-function getRandomPaleValue() {
-
-    return Math.floor(Math.random() * 71) + 150;
-}
-
-const computedColor = () => {
-    // if (!context.value) return "rgba(255, 0, 0,";
-
-    // const hex = color.value.replace(/^#/, "");
-    // // console.log(color.value);
-    // const bigint = Number.parseInt(hex, 16);
-    // const r = (bigint >> 16) & 255;
-    // const g = (bigint >> 8) & 255;
-    // const b = bigint & 255;
-
-
-    // 随机生成 RGB 值
-    const r = getRandomPaleValue();
-    const g = getRandomPaleValue();
-    const b = getRandomPaleValue();
-    return `rgba(${r}, ${g}, ${b},`;
+type Circle = {
+  x: number;
+  y: number;
+  translateX: number;
+  translateY: number;
+  size: number;
+  alpha: number;
+  targetAlpha: number;
+  dx: number;
+  dy: number;
+  magnetism: number;
 };
 
-// 存储当前颜色
-const currentColor = ref<string>(computedColor());
+type Props = {
+  color?: string;
+  quantity?: number;
+  staticity?: number;
+  ease?: number;
+  class?: string;
+};
 
-function setupCanvas(
-    canvas: HTMLCanvasElement,
-    width: number,
-    height: number,
-): {
-    cols: number;
-    rows: number;
-    squares: Float32Array;
-    dpr: number;
-} {
-    const dpr = window.devicePixelRatio || 1;
-    canvas.width = width * dpr;
-    canvas.height = height * dpr;
-    canvas.style.width = `${width}px`;
-    canvas.style.height = `${height}px`;
+const props = withDefaults(defineProps<Props>(), {
+  color: "#06c964",
+  quantity: 200,
+  staticity: 40,
+  ease: 50,
+  class: "",
+});
 
-    const cols = Math.floor(width / (squareSize.value + gridGap.value));
-    const rows = Math.floor(height / (squareSize.value + gridGap.value));
+const canvasRef = ref<HTMLCanvasElement | null>(null);
+const canvasContainerRef = ref<HTMLDivElement | null>(null);
+const context = ref<CanvasRenderingContext2D | null>(null);
+const circles = ref<Circle[]>([]);
+const mouse = reactive<{ x: number; y: number }>({ x: 0, y: 0 });
+const canvasSize = reactive<{ w: number; h: number }>({ w: 0, h: 0 });
+const { x: mouseX, y: mouseY } = useMouse();
+const { pixelRatio } = useDevicePixelRatio();
 
-    const squares = new Float32Array(cols * rows);
-    for (let i = 0; i < squares.length; i++) {
-        squares[i] = Math.random() * maxOpacity.value;
-    }
-    return { cols, rows, squares, dpr };
-}
+const color = computed(() => {
+  // Remove the leading '#' if it's present
+  let hex = props.color.replace(/^#/, "");
 
-function updateSquares(squares: Float32Array, deltaTime: number) {
-    for (let i = 0; i < squares.length; i++) {
-        if (Math.random() < flickerChance.value * deltaTime) {
-            squares[i] = Math.random() * maxOpacity.value;
-        }
-    }
-}
+  // If the hex code is 3 characters, expand it to 6 characters
+  if (hex.length === 3) {
+    hex = hex
+      .split("")
+      .map((char) => char + char)
+      .join("");
+  }
 
-function drawGrid(
-    ctx: CanvasRenderingContext2D,
-    width: number,
-    height: number,
-    cols: number,
-    rows: number,
-    squares: Float32Array,
-    dpr: number,
-) {
-    ctx.clearRect(0, 0, width, height);
-    ctx.fillStyle = "transparent";
-    ctx.fillRect(0, 0, width, height);
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-            const opacity = squares[i * rows + j];
-            ctx.fillStyle = `${currentColor.value}${opacity})`;
-            ctx.fillRect(
-                i * (squareSize.value + gridGap.value) * dpr,
-                j * (squareSize.value + gridGap.value) * dpr,
-                squareSize.value * dpr,
-                squareSize.value * dpr,
-            );
-        }
-    }
-}
+  // Parse the r, g, b values from the hex string
+  const bigint = parseInt(hex, 16);
+  const r = (bigint >> 16) & 255; // Extract the red component
+  const g = (bigint >> 8) & 255; // Extract the green component
+  const b = bigint & 255; // Extract the blue component
 
-const gridParams = ref<ReturnType<typeof setupCanvas>>();
-
-function updateCanvasSize() {
-    const newWidth = width.value || containerRef.value!.clientWidth;
-    const newHeight = height.value || containerRef.value!.clientHeight;
-    console.log(newWidth, newHeight);
-
-    canvasSize.value = { width: newWidth, height: newHeight };
-    gridParams.value = setupCanvas(canvasRef.value!, newWidth, newHeight);
-}
-
-let animationFrameId: number | undefined;
-let resizeObserver: ResizeObserver | undefined;
-let intersectionObserver: IntersectionObserver | undefined;
-let lastTime = 0;
-
-function animate(time: number) {
-    if (!isInView.value) return;
-
-    const deltaTime = (time - lastTime) / 1000;
-    lastTime = time;
-
-    updateSquares(gridParams.value!.squares, deltaTime);
-    drawGrid(
-        context.value!,
-        canvasRef.value!.width,
-        canvasRef.value!.height,
-        gridParams.value!.cols,
-        gridParams.value!.rows,
-        gridParams.value!.squares,
-        gridParams.value!.dpr,
-    );
-    animationFrameId = requestAnimationFrame(animate);
-}
+  // Return the RGB values as a string separated by spaces
+  return `${r} ${g} ${b}`;
+});
 
 onMounted(() => {
-    if (!canvasRef.value || !containerRef.value) return;
-    context.value = canvasRef.value.getContext("2d")!;
-    if (!context.value) return;
+  if (canvasRef.value) {
+    context.value = canvasRef.value.getContext("2d");
+  }
 
-    updateCanvasSize();
+  initCanvas();
+  animate();
+  window.addEventListener("resize", initCanvas);
 
-    resizeObserver = new ResizeObserver(() => {
-        updateCanvasSize();
-    });
-    intersectionObserver = new IntersectionObserver(
-        ([entry]) => {
-            isInView.value = entry!.isIntersecting;
-            animationFrameId = requestAnimationFrame(animate);
-        },
-        { threshold: 0 },
-    );
-
-    resizeObserver.observe(containerRef.value);
-    intersectionObserver.observe(canvasRef.value);
-
-    // 1.5s后插槽加载完毕重新更新容器大小
-    setTimeout(() => {
-        const article = document.querySelector("article")
-        containerRef.value!.style.height = `${article?.clientHeight || 0}px`;
-        updateCanvasSize();
-    }, 1500);
-
-    // 每1秒更换一次颜色
-    setInterval(() => {
-        currentColor.value = computedColor();
-    }, 888);
+  setTimeout(() => {
+    const article = document.querySelector("article");
+    const height = `${article?.clientHeight || 0}px`;
+    canvasContainerRef.value!.style.height = height;
+    initCanvas();
+  }, 1500);
 });
 
 onBeforeUnmount(() => {
-    if (animationFrameId) {
-        cancelAnimationFrame(animationFrameId);
-    }
-    resizeObserver?.disconnect();
-    intersectionObserver?.disconnect();
+  window.removeEventListener("resize", initCanvas);
 });
+
+watch([mouseX, mouseY], () => {
+  onMouseMove();
+});
+
+function initCanvas() {
+  resizeCanvas();
+  drawParticles();
+}
+
+function onMouseMove() {
+  if (canvasRef.value) {
+    const rect = canvasRef.value.getBoundingClientRect();
+    const { w, h } = canvasSize;
+    const x = mouseX.value - rect.left - w / 2;
+    const y = mouseY.value - rect.top - h / 2;
+
+    const inside = x < w / 2 && x > -w / 2 && y < h / 2 && y > -h / 2;
+    if (inside) {
+      mouse.x = x;
+      mouse.y = y;
+    }
+  }
+}
+
+function resizeCanvas() {
+  if (canvasContainerRef.value && canvasRef.value && context.value) {
+    circles.value.length = 0;
+    canvasSize.w = canvasContainerRef.value.offsetWidth;
+    canvasSize.h = canvasContainerRef.value.offsetHeight;
+    canvasRef.value.width = canvasSize.w * pixelRatio.value;
+    canvasRef.value.height = canvasSize.h * pixelRatio.value;
+    canvasRef.value.style.width = canvasSize.w + "px";
+    canvasRef.value.style.height = canvasSize.h + "px";
+    context.value.scale(pixelRatio.value, pixelRatio.value);
+  }
+}
+
+function circleParams(): Circle {
+  const x = Math.floor(Math.random() * canvasSize.w);
+  const y = Math.floor(Math.random() * canvasSize.h);
+  const translateX = 0;
+  const translateY = 0;
+  const size = Math.floor(Math.random() * 2) + 1;
+  const alpha = 0;
+  const targetAlpha = parseFloat((Math.random() * 0.6 + 0.1).toFixed(1));
+  const dx = (Math.random() - 0.5) * 0.2;
+  const dy = (Math.random() - 0.5) * 0.2;
+  const magnetism = 0.1 + Math.random() * 4;
+  return {
+    x,
+    y,
+    translateX,
+    translateY,
+    size,
+    alpha,
+    targetAlpha,
+    dx,
+    dy,
+    magnetism,
+  };
+}
+
+function drawCircle(circle: Circle, update = false) {
+  if (context.value) {
+    const { x, y, translateX, translateY, size, alpha } = circle;
+    context.value.translate(translateX, translateY);
+    context.value.beginPath();
+    context.value.arc(x, y, size, 0, 2 * Math.PI);
+    context.value.fillStyle = `rgba(${color.value
+      .split(" ")
+      .join(", ")}, ${alpha})`;
+    context.value.fill();
+    context.value.setTransform(pixelRatio.value, 0, 0, pixelRatio.value, 0, 0);
+
+    if (!update) {
+      circles.value.push(circle);
+    }
+  }
+}
+
+function clearContext() {
+  if (context.value) {
+    context.value.clearRect(0, 0, canvasSize.w, canvasSize.h);
+  }
+}
+
+function drawParticles() {
+  clearContext();
+  const particleCount = props.quantity;
+  for (let i = 0; i < particleCount; i++) {
+    const circle = circleParams();
+    drawCircle(circle);
+  }
+}
+
+function remapValue(
+  value: number,
+  start1: number,
+  end1: number,
+  start2: number,
+  end2: number
+): number {
+  const remapped =
+    ((value - start1) * (end2 - start2)) / (end1 - start1) + start2;
+  return remapped > 0 ? remapped : 0;
+}
+
+function animate() {
+  clearContext();
+  circles.value.forEach((circle, i) => {
+    // Handle the alpha value
+    const edge = [
+      circle.x + circle.translateX - circle.size, // distance from left edge
+      canvasSize.w - circle.x - circle.translateX - circle.size, // distance from right edge
+      circle.y + circle.translateY - circle.size, // distance from top edge
+      canvasSize.h - circle.y - circle.translateY - circle.size, // distance from bottom edge
+    ];
+
+    const closestEdge = edge.reduce((a, b) => Math.min(a, b));
+    const remapClosestEdge = parseFloat(
+      remapValue(closestEdge, 0, 20, 0, 1).toFixed(2)
+    );
+
+    if (remapClosestEdge > 1) {
+      circle.alpha += 0.02;
+      if (circle.alpha > circle.targetAlpha) circle.alpha = circle.targetAlpha;
+    } else {
+      circle.alpha = circle.targetAlpha * remapClosestEdge;
+    }
+
+    circle.x += circle.dx;
+    circle.y += circle.dy;
+    circle.translateX +=
+      (mouse.x / (props.staticity / circle.magnetism) - circle.translateX) /
+      props.ease;
+    circle.translateY +=
+      (mouse.y / (props.staticity / circle.magnetism) - circle.translateY) /
+      props.ease;
+
+    // circle gets out of the canvas
+    if (
+      circle.x < -circle.size ||
+      circle.x > canvasSize.w + circle.size ||
+      circle.y < -circle.size ||
+      circle.y > canvasSize.h + circle.size
+    ) {
+      // remove the circle from the array
+      circles.value.splice(i, 1);
+      // create a new circle
+      const newCircle = circleParams();
+      drawCircle(newCircle);
+      // update the circle position
+    } else {
+      drawCircle(
+        {
+          ...circle,
+          x: circle.x,
+          y: circle.y,
+          translateX: circle.translateX,
+          translateY: circle.translateY,
+          alpha: circle.alpha,
+        },
+        true
+      );
+    }
+  });
+  window.requestAnimationFrame(animate);
+}
 </script>
